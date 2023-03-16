6349435c66e56a5fb7b586cab7979d192e56cb5a
Consue <c.anguitasalinas@gmail.com>
2023-03-15 23:17:03 -0300

    feat: sidebar componentization

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -36,11 +36,6 @@ export const FormUser = () => {
   return (
     <div className="flex min-h-full h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          className="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Licencias Médicas</h2>
       </div>
 
